873067404e4058fcd331ea40cb642f6c75bd25a5
Tom Bukovczan <tombukowczan@gmail.com>
2018-12-10 20:07:07 +0100

    Cards

---
+++ Assets/Scripts/Card Scripts/CardScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour,ICard {
+
+	public void Draw()
+	{
+		
+	}
+
+	public void Play()
+	{
+		
+	}
+
+ 
+	void Start()
+    {
+		
+	}
+	
+	void Update()
+    {
+		
+	}
+}
